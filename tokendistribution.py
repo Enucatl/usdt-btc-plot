d8c1e5a5c2146fd94eb3b02b70f81f71b166585a
Matteo Abis <gmatteo..abis@gmail.com>
2017-11-08 23:00:35 +0100

    catch a few errors

--- tokendistribution.py
+++ tokendistribution.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 @click.argument("nodes", type=click.File("w"))
 @click.argument("links", type=click.File("w"))
 def main(verbose, nodes, links):
+    current_node = 0
     logging.config.dictConfig(log_config(verbose))
     node_writer = csv.writer(nodes)
     node_writer.writerow([
@@ -33,46 +34,64 @@ def main(verbose, nodes, links):
     ])
     known_nodes = set()
     new_nodes = set(["3MbYQMMmSkC3AgWkj9FMo5LsPTW1zBTwXL"])
+    # new_nodes = set(["1KYiKJEfdJtap9QX2v9BXJMpz2SfU4pgZw"])
     while new_nodes:
         logger.debug("found %s new nodes", len(new_nodes))
         node = new_nodes.pop()
         url = "http://omniexplorer.info/ask.aspx"
         params = {
-            "api": "getsenderhistory",
+            "api": "gethistory",
             "address": node,
         }
+        logger.debug("requesting %s", params)
         response = requests.get(url, params=params).json()
+        logger.debug("got response %s", response)
         transactions = response["transactions"]
         params = {
             "api": "getbalance",
             "prop": 31,
             "address": node,
         }
+        time.sleep(1)
         balance = requests.get(url, params=params).json()
+        logger.debug("got response %s", response)
         node_writer.writerow([node, balance])
         known_nodes.update([node])
         logger.debug("found %s transactions", len(transactions))
         for transaction in transactions:
-            time.sleep(0.2)
+            time.sleep(1)
             params = {
                 "api": "gettx",
                 "txid": transaction
             }
             response = requests.get(url, params=params)
-            t = json.loads("{" + response.text + "}")
-            logger.debug("transaction %s", t)
+            try:
+                logger.debug("transaction %s", response.text)
+                t = json.loads("{" + response.text + "}")
+            except JSONDecodeError:
+                continue
             # keep only last two batches of 25 + 20 MUSDT
-            if t["blocktime"] < 1509742213:
-                break
-            txid = t["txid"]
-            source = t["sendingaddress"]
-            target = t["referenceaddress"]
-            amount = t["amount"]
+            try:
+                blocktime = t["blocktime"]
+                if blocktime < 1509742213:
+                    break
+                txid = t["txid"]
+                source = t["sendingaddress"]
+                target = t["referenceaddress"]
+                amount = float(t["amount"])
+            except KeyError:
+                continue
+            if source != node or amount < 100000:
+                continue
             link_writer.writerow([
                 txid, source, target, amount
             ])
             if target not in known_nodes:
                 new_nodes.update([target])
+        time.sleep(10)
+        logger.debug("new nodes: %s", new_nodes)
+        current_node += 1
+        logger.debug("analized %s nodes", current_node)
 
 
 if __name__ == "__main__":
